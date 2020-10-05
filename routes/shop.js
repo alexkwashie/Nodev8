6669473c0a801e06263af10918df95405de71a44
Alex A. Kwashie <alex.kwashie@hotmail.com>
2020-10-05 01:14:34 +0100

    installing express-handlebars for templating

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,16 @@ const express = require('express');
 
 const router = express.Router();
 
+//import admin.js to access the products data
+const adminData = require('./admin');
+
+
+
+
+
+
 router.get('/', (req, res, next) => {
+    console.log('shop.js', adminData.products);
     res.sendFile(path.join(__dirname, '../','views', 'shop.html'));
 });
 
