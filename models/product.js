a8e08f0686e30e17e647acd1e06c99ebb27195b2
Alex A. Kwashie <alex.kwashie@hotmail.com>
2020-10-15 21:23:12 +0100

    storing input data in a file on local machine

--- models/product.js
+++ models/product.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
 
 //Create an export class to define product properties
 module.exports = class Product{
@@ -7,11 +9,36 @@ module.exports = class Product{
     }
 
     save() {
-        products.push(this);
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+        //read the file to access it
+        fs.readFile(p, (err, fileContent) => {
+            let products = [];
+
+            if (!err){
+                products = JSON.parse(fileContent); //convert the file content to a javascript array type.
+            }
+
+            //Append the information from the webpage to the fileContent which is the products array.
+            products.push(this);
+
+            //Save to the file
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err);
+            });
+
+        });
     }
 
     //More on static - https://www.w3schools.com/jsref/jsref_class_static.asp
     static fetchAll(){
-        return products;
+        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+        fs.readFile(p, (err, fileContent) =>{
+            if(err){
+                return [];
+            }
+
+            return JSON.parse(fileContent);
+        })
     }
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,4 @@
-const path = require('path');
+
 const express = require('express');
 
 const productsController = require('../controller/products');
