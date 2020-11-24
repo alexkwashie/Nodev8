d7d43e23aee3eb5a97047a3f0b24b724aa797b09
Alex A. Kwashie <alex.kwashie@hotmail.com>
2020-11-24 01:17:29 +0000

    adding prodts to admin ctrla and populating the edit page

--- controller/admin.js
+++ controller/admin.js
@@ -1,50 +1,58 @@
-
 const Products = require('../models/product');
 
-exports.getAddProduct = (req, res, next) =>{
+exports.getAddProduct = (req, res, next) => {
 
     res.render('admin/edit-product', {
         pageTitle: 'Add Product',
-        path: 'admin/add-product'
-        //layout: true //by this being true it will not use the default main handlebar file but allows u to assign a different path/location
+        path: 'admin/add-product',
+        editing:false
+        // layout: true //by this being true it will not use the default main handlebar
+        // file but allows u to assign a different path/location
     });
 };
 
-exports.postProduct = (req, res, next)=>{
+exports.postProduct = (req, res, next) => {
     const title = req.body.title;
     const imageUrl = req.body.imageUrl;
     const description = req.body.description;
     const price = req.body.price;
-    const product = new Products(title, imageUrl, description, price);  //create a new object from the class blueprint
+    const product = new Products(title, imageUrl, description, price); //create a new object from the class blueprint
     product.save();
     console.log(req.body)
     res.redirect('/');
 
 };
 
-exports.getEditProduct = (req, res, next) =>{
-    const editMode = req.query.edit;
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit; // this forom the url (..?edit=)
     //console.log(editMode);
-    if(!editMode){
+    if (!editMode) {
         return res.redirect('/');
     }
-    res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: 'admin/edit-product',
-        editing: editMode,
-        //layout: true //by this being true it will not use the default main handlebar file but allows u to assign a different path/location
+    const prodId = req.params.productId; // "productId" is from the admin routea file
+    Products.findbyId(prodId, product => {
+        if (!product) {
+            return req.redirect('/')
+        }
+        res.render('admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: 'admin/edit-product',
+            editing: editMode,
+            product: product
+        });
     });
 };
 
 exports.getProducts = (req, res, next) => {
-    Products.fetchAll( products =>{
+    Products.fetchAll(products => {
         res.render('admin/product-adminview', {
-            prods:products,
+            prods: products,
             pageTitle: 'Admin Products Page',
             path: '/admin-products',
             hasProducts: products.length > 0,
             activeShop: true,
-            //layout: true //by this being true it will not use the default main handlebar file but allows u to assign a different path/location
+            // layout: true //by this being true it will not use the default main handlebar
+            // file but allows u to assign a different path/location
         });
     });
 
